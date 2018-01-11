8084d9753ef673c9ace0265ec075b6f19260efad
wywongbd <emarald10@gmail.com>
2018-01-11 13:51:59 +0800

    completed squeezeNet class

--- squeezeNet.py
+++ squeezeNet.py
@@ -5,8 +5,8 @@ class squeezelayers:
 	Implementation of squeezelayers model demonstrated in the paper: https://arxiv.org/abs/1602.07360
 	
 	"""
-	def __init__(self, session, alpha, optimizer=tf.train.GradientDescentOptimizer, squeeze_ratio=1):
-        self.dropout   = tf.placeholder(tf.float32)
+	def __init__(self, session, alpha, optimizer=tf.train.GradientDescentOptimizer, squeeze_ratio = 0.125):
+		self.sess = session
         self.target    = tf.placeholder(tf.float32, [None, 1000])
         self.imgs      = tf.placeholder(tf.float32, [None, 224, 224, 3])
 
@@ -19,7 +19,7 @@ class squeezelayers:
 
         self.build_model()
         self.init_optimizer()
-        self.init_model()
+        self.train_model()
 
     def build_model(self):
     	layers = {}
@@ -31,46 +31,46 @@ class squeezelayers:
 
         layers['input'] = images
 
-        # conv1_1
-        layers['conv1'] = self.conv_layer('conv1', layers['input'],
-                              W=self.weight_variable([3, 3, 3, 64], name='conv1_w'), stride=[1, 2, 2, 1])
+        layers['conv1'] = tf.nn.conv2d(layers['input'], self.get_weight([3, 3, 3, 64], 'conv1'), stride = [1, 2, 2, 1], padding= 'SAME')
+        layers['relu1'] = tf.nn.relu(tf.nn.bias_add(layers['conv1'], self.get_bias([64], 'relu1')))
 
-        layers['relu1'] = self.relu_layer('relu1', layers['conv1'], b=self.bias_variable([64], 'relu1_b'))
-        layers['pool1'] = self.pool_layer('pool1', layers['relu1'])
+        layers['pool1'] = tf.nn.max_pool(layers['relu1'], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-        layers['fire2'] = self.fire_module('fire2', layers['pool1'], self.sq_ratio * 16, 64, 64)
-        layers['fire3'] = self.fire_module('fire3', layers['fire2'], self.sq_ratio * 16, 64, 64,   True)
-        layers['pool3'] = self.pool_layer('pool3', layers['fire3'])
+        layers['fire2'] = self.fire_module('fire2', layers['pool1'], self.sq_ratio * 128, 64, 64)
+        layers['fire3'] = self.fire_module('fire3', layers['fire2'], self.sq_ratio * 128, 64, 64)
 
-        layers['fire4'] = self.fire_module('fire4', layers['pool3'], self.sq_ratio * 32, 128, 128)
-        layers['fire5'] = self.fire_module('fire5', layers['fire4'], self.sq_ratio * 32, 128, 128, True)
-        layers['pool5'] = self.pool_layer('pool5', layers['fire5'])
+        layers['pool3'] = tf.nn.max_pool(layers['fire3'], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-        layers['fire6'] = self.fire_module('fire6', layers['pool5'], self.sq_ratio * 48, 192, 192)
-        layers['fire7'] = self.fire_module('fire7', layers['fire6'], self.sq_ratio * 48, 192, 192, True)
-        layers['fire8'] = self.fire_module('fire8', layers['fire7'], self.sq_ratio * 64, 256, 256)
-        layers['fire9'] = self.fire_module('fire9', layers['fire8'], self.sq_ratio * 64, 256, 256, True)
+        layers['fire4'] = self.fire_module('fire4', layers['pool3'], self.sq_ratio * 256, 128, 128)
+        layers['fire5'] = self.fire_module('fire5', layers['fire4'], self.sq_ratio * 384, 192, 192)
 
-        # 50% dropout
-        layers['dropout9'] = tf.nn.dropout(layers['fire9'], self.dropout)
-        layers['conv10']   = self.conv_layer('conv10', layers['dropout9'],
-                               W=self.weight_variable([1, 1, 512, 1000], name='conv10', init='normal'))
-        layers['relu10'] = self.relu_layer('relu10', layers['conv10'], b=self.bias_variable([1000], 'relu10_b'))
-        layers['pool10'] = self.pool_layer('pool10', layers['relu10'], pooling_type='avg')
+        layers['pool5'] = tf.nn.max_pool(layers['fire5'], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-        avg_pool_shape        = tf.shape(layers['pool10'])
-        layers['pool_reshaped']  = tf.reshape(layers['pool10'], [avg_pool_shape[0],-1])
-        self.fc2              = layers['pool_reshaped']
-        self.logits           = layers['pool_reshaped']
+        layers['fire6'] = self.fire_module('fire6', layers['pool5'], self.sq_ratio * 384, 192, 192)
+        layers['fire7'] = self.fire_module('fire7', layers['fire6'], self.sq_ratio * 384, 192, 192)
+        layers['fire8'] = self.fire_module('fire8', layers['fire7'], self.sq_ratio * 512, 256, 256)
+        layers['fire9'] = self.fire_module('fire9', layers['fire8'], self.sq_ratio * 512, 256, 256)
 
+        layers['conv10'] = tf.nn.conv2d(layers['fire9'], self.get_weight([1, 1, 512, 1000], 'conv10'), stride = [1, 1, 1, 1], padding= 'VALID')
+        layers['relu10'] = tf.nn.relu(tf.nn.bias_add(layers['conv10'], self.get_bias([512], 'relu10')))
+
+        layers['avgpool10'] = tf.nn.avg_pool(layers['relu10'], ksize=[1, 13, 13, 1], strides=[1, 1, 1, 1], padding='VALID')
+
+        avg_pool_shape = tf.shape(layers['pool10'])
+        layers['pool_reshaped'] = tf.reshape(layers['pool10'], [avg_pool_shape[0],-1])
+
+        self.logits = layers['pool_reshaped']
         self.probs = tf.nn.softmax(self.logits)
-        self.layers   = layers
+
+        self.layers = layers
 
     def init_optimizer(self):
+    	self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.target))
+        self.optimize = self.optimizer(self.alpha).minimize(self.loss)
 
-
-    def init_model(self):
-
+    def train_model(self, X_train, Y_train):
+    	self.sess.run(tf.global_variables_initializer())
+    	self.sess.run(self.optimize, feed_dict={self.imgs: X_train, self.target: Y_train})
 
     def get_weight(self, shape, name, initializer = 'truncated_normal'):
     	if initializer == 'truncated_normal':
@@ -100,13 +100,13 @@ class squeezelayers:
 		N, H, W, C =  input.get_shape()
 
 		squeeze = tf.nn.conv2d(input, self.get_weight([1, 1, C, s_1x1], name + '_squeeze_1x1'), stride = [1, 1, 1, 1], padding= 'SAME') 
-		squeeze = tf.nn.relu(tf.nn.bias_add(squeeze, get_bias([s_1x1], name + '_squeeze_1x1')))
+		squeeze = tf.nn.relu(tf.nn.bias_add(squeeze, self.get_bias([s_1x1], name + '_squeeze_1x1')))
 
 		expand1 = tf.nn.conv2d(squeeze, self.get_weight([1, 1, s_1x1, e_1x1], name + '_expand_1x1'), stride = [1, 1, 1, 1], padding= 'SAME') 
-		expand1 = tf.nn.relu(tf.nn.bias_add(squeeze, get_bias([e_1x1], name + '_expand_1x1')))
+		expand1 = tf.nn.relu(tf.nn.bias_add(squeeze, self.get_bias([e_1x1], name + '_expand_1x1')))
 
 		expand2 = tf.nn.conv2d(squeeze, self.get_weight([1, 1, s_1x1, e_3x3], name + '_expand_3x3'), stride = [1, 1, 1, 1], padding= 'SAME') 
-		expand2 = tf.nn.relu(tf.nn.bias_add(expand2, get_bias([e_3x3], name + '_expand_3x3')))
+		expand2 = tf.nn.relu(tf.nn.bias_add(expand2, self.get_bias([e_3x3], name + '_expand_3x3')))
 
 		result = tf.concat([expand1, expand3], 3)
 		result = tf.nn.relu(result)
